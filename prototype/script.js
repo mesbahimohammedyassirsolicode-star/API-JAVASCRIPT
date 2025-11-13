7fe0901f2875cf389bf0cc01f9586dbf0443a035
mesbahimohammedyassirsolicode-star <mesbahi.mohammedyassir.solicode@gmail.com>
2025-11-13 23:02:39 +0100

    add

--- prototype/script.js
+++ prototype/script.js
@@ -1,10 +1,17 @@
-let container=document.getElementById("main");
+const container = document.getElementById("cardContainer");
 fetch("https://countries-api-hsak.onrender.com/api/countries")
   .then(response => response.json())
   .then(data => {
     data.forEach(p => {
         let card =document.createElement("div")
-        card.innerHTML =
-        
+        card.classList.add("card");
+        card.innerHTML = `
+        <h2>${p.name}</h2>
+        <p><strong>Capital:</strong> ${p.capital}</p>
+        <p><strong>language:</strong> ${p.language}</p>
+        <img class="flag-img" src = "${p.flag}")></img>
+        `;
+        container.appendChild(card);
     });
-  }
+  });
+  
