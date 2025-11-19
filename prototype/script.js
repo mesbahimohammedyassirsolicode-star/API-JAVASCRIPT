b9de9ce8e92140411337307e3d9606f3f3eb9508
https://github.com/mesbahimohammedyassirsolicode-star <mesbahi.mohammedyassir.solicode@gmail.com>
2025-11-19 15:42:53 +0100

    update

--- prototype/script.js
+++ prototype/script.js
@@ -2,17 +2,13 @@ let container = document.getElementById("cardContainer");
 let continentSelect = document.querySelector(".my-select");
 let countriesData = [];
 
-// Fetch and display all countries
+// Fetch 
 fetch("https://countries-api-hsak.onrender.com/api/countries")
   .then(response => response.json())
   .then(data => {
     countriesData = data;
     displayCountries(data);
   })
-  .catch(error => {
-    console.error('Error fetching countries:', error);
-  });
-
 // Function to display countries
 function displayCountries(countries) {
   container.innerHTML = '';
@@ -25,22 +21,19 @@ function displayCountries(countries) {
       <p><strong>Capital:</strong> ${country.capital}</p>
       <p><strong>Language:</strong> ${country.language}</p>
       <p><strong>Continent:</strong> ${country.continent}</p>
-      <img class="flag-img" src="${country.flag}" alt="${country.name} flag">
+      <img class="flag-img" src="${country.flag}"flag">
     `;
     container.appendChild(card);
   });
 }
-
-// Add event listener to the continent select dropdown
+//filter the countiries 
 continentSelect.addEventListener('change', function() {
   const selectedContinent = this.value;
   
   if (selectedContinent === "continent") {
-    // If "continent" is selected (the hidden option), show all countries
     displayCountries(countriesData);
   } else {
-    // Filter countries by selected continent
-    const filteredCountries = countriesData.filter(country => 
+  const filteredCountries = countriesData.filter(country => 
       country.continent === selectedContinent
     );
     displayCountries(filteredCountries);
